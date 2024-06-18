b73ff515cbd6b73b55811884dc2b794961590b3a
Isaiah Harville <harvillerisaiah@gmail.com>
2024-06-18 02:41:01 +0000

    setup linux dev environment

--- poker/setup.py
+++ poker/setup.py
@@ -1,7 +1,15 @@
+import platform
 from setuptools import setup, find_packages
 
-def requirementsin():
-    with open("requirements.txt", "r") as f:
+def get_requirements():
+    os_type = platform.system()
+    
+    if os_type == "Windows":
+        requirements_file = "requirements-win.txt"
+    else:
+        requirements_file = "requirements-linux.txt"
+    
+    with open(requirements_file, "r") as f:
         return [line.strip() for line in f if line and not line.startswith('#')]
 
 setup(
@@ -9,7 +17,7 @@ setup(
     version='0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=requirementsin(),
+    install_requires=get_requirements(),
     include_package_data=True,
     package_data={'' : ['tests/*.py']},
     entry_points={
