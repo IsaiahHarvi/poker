675f86d19cdf21a473a0c5bc5abe32e3a6b475ae
Isaiah Harville <harvillerisaiah@gmail.com>
2024-06-27 17:30:47 +0000

    make devcontainers more robust and finish windows testing

--- .devcontainer/setup_devcontainer.sh
+++ .devcontainer/setup_devcontainer.sh
@@ -5,9 +5,9 @@ cd ./poker
 pip3 install -e .
 
 # ./.devcontainer/setup_git.sh
-if [ -f .github-token ]; then
+if [ -f /workspaces/poker/.devcontainer/.github-token ]; then
   echo "GitHub token found, proceeding with authentication..."
-  gh auth login --with-token < .github-token
+  gh auth login --with-token < /workspaces/poker/.devcontainer/.github-token
   gh auth status || echo "GitHub auth failed, check your permissions."
 else
   echo "No GitHub token found, skipping authentication..."
